27682f58d78061b4faeead97734e0078eef39611
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-07-31 17:26:41 +0500

    Update text.js

--- jsFolder/text.js
+++ jsFolder/text.js
@@ -1,6 +1,10 @@
-console.log("Let's build a pyramid using JavaScript basics:");
-console.log("    #    ");
-console.log("   ###   ");
-console.log("  #####  ");
-console.log(" ####### ");
-console.log("#########");
+// day 1
+// console.log("Let's build a pyramid using JavaScript basics:");
+// console.log("    #    ");
+// console.log("   ###   ");
+// console.log("  #####  ");
+// console.log(" ####### ");
+// console.log("#########");
+
+// day 2
+let character;
