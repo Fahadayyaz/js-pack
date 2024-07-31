21e9dcd509ac2657ad87235269b267f5a99b738e
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-07-31 17:59:27 +0500

    Update text.js

--- jsFolder/text.js
+++ jsFolder/text.js
@@ -1,4 +1,4 @@
-// Step 1
+// Step 1: print on the console
 // console.log("Let's build a pyramid using JavaScript basics:");
 // console.log("    #    ");
 // console.log("   ###   ");
@@ -6,26 +6,31 @@
 // console.log(" ####### ");
 // console.log("#########");
 
-// Step 2
+// Step 2: declare a variable
 // let character;
 
-// Step 3
+// Step 3: pass value to the "character" variable
 // let character = "Hello";
 
-// Step 4
+// Step 4: single quotes
 // let character = 'Hello';
 
-// Step 5
+// Step 5: print the value of the variable on console
 // let character = "Hello";
 // console.log(character);
 
-// Step 6
+// Step 6: change the value of the character
 // let character = "Hello";
 // character = "World";
 // console.log(character);
 
-// Step 7
-let character = "Hello";
-console.log(character);
-character = "World";
-console.log(character);
+// Step 7: print both values
+// let character = "Hello";
+// console.log(character);
+// character = "World";
+// console.log(character);
+
+// Step 8: write variable using camelCase technique
+// let secondCharacter;
+
+// Step 9:
