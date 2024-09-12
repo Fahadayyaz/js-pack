60cd352b02e180146eb82d880875e29c30839ba5
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-09-12 13:05:48 +0500

    Update text.js

--- jsFolder/text.js
+++ jsFolder/text.js
@@ -1,43 +1,43 @@
-// Step 1: print on the console
-// console.log("Let's build a pyramid using JavaScript basics:");
-// console.log("    #    ");
-// console.log("   ###   ");
-// console.log("  #####  ");
-// console.log(" ####### ");
-// console.log("#########");
+Step 1: print on the console
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("    #    ");
+console.log("   ###   ");
+console.log("  #####  ");
+console.log(" ####### ");
+console.log("#########");
 
-// Step 2: declare a variable
-// let character;
+Step 2: declare a variable
+let character;
 
-// Step 3: pass value to the "character" variable
-// let character = "Hello";
+Step 3: pass value to the "character" variable
+let character = "Hello";
 
-// Step 4: single quotes
-// let character = 'Hello';
+Step 4: single quotes
+let character = 'Hello';
 
-// Step 5: print the value of the variable on console
-// let character = "Hello";
-// console.log(character);
+Step 5: print the value of the variable on console
+let character = "Hello";
+console.log(character);
 
-// Step 6: change the value of the character
-// let character = "Hello";
-// character = "World";
-// console.log(character);
+Step 6: change the value of the character
+let character = "Hello";
+character = "World";
+console.log(character);
 
-// Step 7: print both values
-// let character = "Hello";
-// console.log(character);
-// character = "World";
-// console.log(character);
+Step 7: print both values
+let character = "Hello";
+console.log(character);
+character = "World";
+console.log(character);
 
-// Step 8: write variable using camelCase technique
-// let secondCharacter;
+Step 8: write variable using camelCase technique
+let secondCharacter;
 
-// Step 9: due to no value in the variable this will show undefined
-// let secondCharacter;
-// console.log(secondCharacter);
+Step 9: due to no value in the variable this will show undefined
+let secondCharacter;
+console.log(secondCharacter);
 
-// Step 10: Assign "Test" value to the secondCharacter
-// let secondCharacter;
-// secondCharacter = "Test";
-// console.log(secondCharacter);
+Step 10: Assign "Test" value to the secondCharacter
+let secondCharacter;
+secondCharacter = "Test";
+console.log(secondCharacter);
